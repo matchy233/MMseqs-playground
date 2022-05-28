62f357b67d2124284983289f56a9daa4afef162e
matchy233 <matchy233@gmail.com>
2022-05-28 15:45:27 +0900

    Format old code

--- src/playground/kmer2long.cpp
+++ src/playground/kmer2long.cpp
@@ -4,29 +4,32 @@
 #include <LocalParameters.h>
 #include <SubstitutionMatrix.h>
 
-int kmer2long(int argc, const char **argv, const Command &command) {
-  LocalParameters &par = LocalParameters::getLocalInstance();
-  par.parseParameters(argc, argv, command, true, 0,
-                      LocalParameters::PARSE_VARIADIC);
+int kmer2long(int argc, const char **argv, const Command &command)
+{
+    LocalParameters &par = LocalParameters::getLocalInstance();
+    par.parseParameters(argc, argv, command, true, 0,
+                        LocalParameters::PARSE_VARIADIC);
 
-  BaseMatrix *subMat = new SubstitutionMatrix(
-      par.seedScoringMatrixFile.values.aminoacid().c_str(), 8.0, -0.2f);
-  Indexer idx(subMat->alphabetSize - 1, 9);
+    BaseMatrix *subMat = new SubstitutionMatrix(
+        par.seedScoringMatrixFile.values.aminoacid().c_str(), 8.0, -0.2f);
+    Indexer idx(subMat->alphabetSize - 1, 9);
 
-  if (argc < 0) {
-    Debug(Debug::ERROR)
-        << "Too few arguments: please provide some inputs for conversion\n";
-    return EXIT_FAILURE;
-  }
+    if (argc < 0)
+    {
+        Debug(Debug::ERROR)
+            << "Too few arguments: please provide some inputs for conversion\n";
+        return EXIT_FAILURE;
+    }
 
-  for (int i = 0; i < argc; i++) {
-    Debug(Debug::INFO) << "Processing " << argv[i] << "\n";
-    const unsigned char *kmer = (const unsigned char *)argv[i];
-    size_t kmerAsLong = idx.int2index(kmer, 0, 9);
-    Debug(Debug::INFO) << kmerAsLong << "\n";
-    // Debug(Debug::INFO) << "Back conversion: ";
-    // idx.printKmer(kmerAsLong, 9, subMat->num2aa);
-    // Debug(Debug::INFO) << "\n";
-  }
-  return EXIT_SUCCESS;
+    for (int i = 0; i < argc; i++)
+    {
+        Debug(Debug::INFO) << "Processing " << argv[i] << "\n";
+        const unsigned char *kmer = (const unsigned char *)argv[i];
+        size_t kmerAsLong = idx.int2index(kmer, 0, 9);
+        Debug(Debug::INFO) << kmerAsLong << "\n";
+        Debug(Debug::INFO) << "Back conversion: ";
+        idx.printKmer(kmerAsLong, 9, subMat->num2aa);
+        Debug(Debug::INFO) << "\n";
+    }
+    return EXIT_SUCCESS;
 }
--- src/playground/long2kmer.cpp
+++ src/playground/long2kmer.cpp
@@ -5,27 +5,30 @@
 #include <SubstitutionMatrix.h>
 #include <Util.h>
 
-int long2kmer(int argc, const char **argv, const Command &command) {
-  LocalParameters &par = LocalParameters::getLocalInstance();
-  par.parseParameters(argc, argv, command, true, 0,
-                      LocalParameters::PARSE_VARIADIC);
+int long2kmer(int argc, const char **argv, const Command &command)
+{
+    LocalParameters &par = LocalParameters::getLocalInstance();
+    par.parseParameters(argc, argv, command, true, 0,
+                        LocalParameters::PARSE_VARIADIC);
 
-  BaseMatrix *subMat = new SubstitutionMatrix(
-      par.seedScoringMatrixFile.values.aminoacid().c_str(), 2.0, -0.2f);
-  Indexer idx(subMat->alphabetSize - 1, 9);
+    BaseMatrix *subMat = new SubstitutionMatrix(
+        par.seedScoringMatrixFile.values.aminoacid().c_str(), 2.0, -0.2f);
+    Indexer idx(subMat->alphabetSize - 1, 9);
 
-  if (argc < 0) {
-    Debug(Debug::ERROR)
-        << "Too few arguments: please provide some inputs for conversion\n";
-    return EXIT_FAILURE;
-  }
+    if (argc < 0)
+    {
+        Debug(Debug::ERROR)
+            << "Too few arguments: please provide some inputs for conversion\n";
+        return EXIT_FAILURE;
+    }
 
-  for (int i = 0; i < argc; i++) {
-    Debug(Debug::INFO) << "Processing " << argv[i] << "\n";
-    size_t kmeraAsLong = Util::fast_atoi<size_t>(argv[i]);
-    Debug(Debug::INFO) << "kmeraAsLong: " << kmeraAsLong << "\n";
-    idx.printKmer(kmeraAsLong, 9, subMat->num2aa);
-    Debug(Debug::INFO) << "\n";
-  }
-  return EXIT_SUCCESS;
+    for (int i = 0; i < argc; i++)
+    {
+        Debug(Debug::INFO) << "Processing " << argv[i] << "\n";
+        size_t kmeraAsLong = Util::fast_atoi<size_t>(argv[i]);
+        Debug(Debug::INFO) << "kmeraAsLong: " << kmeraAsLong << "\n";
+        idx.printKmer(kmeraAsLong, 9, subMat->num2aa);
+        Debug(Debug::INFO) << "\n";
+    }
+    return EXIT_SUCCESS;
 }
